35b7dd96908812f9f22386588cf2a79c4ca1da64
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-04-08 15:06:44 -0300

    componentes de login 6

--- components/LoginForm/LoginForm.jsx
+++ components/LoginForm/LoginForm.jsx
@@ -1,48 +1,50 @@
-import {useState } from 'react';
-import useCsfrToken from '../Hooks/useCsrfToken';
+import { useState } from "react";
+import useCsfrToken from "../Hooks/useCsrfToken";
 
-export default function LoginForm(){
-    const csrfToken = useCsfrToken();
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [mensagem, setMensagem] = useState("");
+export default function LoginForm() {
+  const csrfToken = useCsfrToken();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [mensagem, setMensagem] = useState("");
 
-   const handleLogin = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
-    try{
-        const res = await fetch("http://localhost:3000/api/users/login", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-type": "aplication/json",
-                "csrf-token": csrfToken
-            },
-            body: JSON.stringify({ email, senha})
-        });
+    try {
+      const res = await fetch("http://localhost:3000/api/users/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-type": "aplication/json",
+          "csrf-token": csrfToken,
+        },
+        body: JSON.stringify({ email, senha }),
+      });
 
-        const data = await res.json();
-        if(res.ok){
-            setMensagem("Logim bem sucedido")
-        }{
-            setMensagem(`${data.error} erro ao fazer login`)
-        }
-
-    }catch(error){
-        console.log(error)
+      const data = await res.json();
+      if (res.ok) {
+        setMensagem("Logim bem sucedido");
+      }
+      {
+        setMensagem(`${data.error} erro ao fazer login`);
+      }
+    } catch (error) {
+      console.log(error);
     }
-   }
+  };
 
-   return (
-    <form onSubmit={handleLogin} style={{
-        display:"flex",
-        flexDirection:"column"
-    }}>
-<div>
-    <label>email</label>
-    <input type="email" value={email}/>
-</div>
-
+  return (
+    <form
+      onSubmit={handleLogin}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <label>email</label>
+        <input type="email" value={email} />
+      </div>
     </form>
-   )
+  );
 }
